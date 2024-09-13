3c9fe8f90124862744732097615f57956010d5a7
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-09-13 05:54:47 +0000

    adelante algo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [newTodo, setNewTodo] = useState("Taskbychance");
+	const [list, setList] = useState(["pilot test1", "pilot test2"])
+
+
+	const holaClickeame = () => {
+		console.log("Hola bebe");
+		setList([...list, newTodo]);
+
+	}
+
+	const handleChange = (event) => {
+
+		setNewTodo(event.target.value)
+
+	}
+	const deleteTask = (indice) => {
+		const newList = list.filter((todo, i) => i !== indice)
+		setList(newList);
+
+
+	}
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">
+				Fetch and react
+			</h1>
+			<div>
+				<input type="text" onChange={handleChange} />
+				<button onClick={holaClickeame}  >
+					Add task
+				</button>
+			</div>
+			<p>New Task:{newTodo} </p>
+			<ul>
+				{list.map((list, indice) => {
+					return (
+						<li>
+							{list}<button onClick={() => deleteTask()}>Erase</button>
+
+						</li>
+					)
+				})}
+
+			</ul>
 		</div>
-	);
-};
+	)
+}
+
 
 export default Home;
