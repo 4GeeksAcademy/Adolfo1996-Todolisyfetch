8d68dd2e721d5530e16286ad2a410141d4b32923
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-09-17 03:31:52 +0000

    falta el delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 
 	const [newTodo, setNewTodo] = useState("Taskbychance");
-	const [list, setList] = useState(["pilot test1", "pilot test2"])
+	const [list, setList] = useState([])
 
 
 	const holaClickeame = () => {
@@ -21,29 +21,119 @@ const Home = () => {
 		setNewTodo(event.target.value)
 
 	}
-	const deleteTask = (indice) => {
-		const newList = list.filter((todo, i) => i !== indice)
+	const deleteTask = (index) => {
+		const newList = list.filter((todo, i) => i !== index)
 		setList(newList);
 
 
 	}
+
+	const createUser = () => {
+		fetch('https://playground.4geeks.com/todo/users/Adolfo1996', {
+			method: "POST",
+			
+		  })
+		  .then(resp => {
+			  console.log(resp.ok); // Será true si la respuesta es exitosa
+			  console.log(resp.status); // El código de estado 200, 300, 400, etc.
+			  console.log(resp.text()); // Intentará devolver el resultado exacto como string
+			  return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+		  })
+		  .then(data => {
+			  // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+			  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+		  })
+		  .catch(error => {
+			  // Manejo de errores
+			  console.log(error);
+		  });
+	} 
+	const getList = async () =>{
+		try {
+			const response = await fetch ('https://playground.4geeks.com/todo/users/Adolfo1996')
+			const data = await response.json()
+			if(response.status==404) {
+				createUser()
+				
+			}
+		    if(response.ok){
+				setList(data.todos)
+			}
+		} catch (error){}
+	}
+
+	//
+	// const getList = () => {
+	// 	fetch('https://playground.4geeks.com/todo/users/Adolfo1996')
+	// 	  .then(resp => {
+	// 		if(resp.status ==404) {
+	// 			createUser()
+	// 		}
+	// 		if(resp.ok){
+	// 			return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+
+	// 		} 
+	// 		  console.log(resp.ok); // Será true si la respuesta es exitosa
+	// 		  console.log(resp.status); // El código de estado 200, 300, 400, etc.
+	// 		  console.log(resp.text()); // Intentará devolver el resultado exacto como string
+	// 	  })
+	// 	  .then(data => {
+	// 		if(data){
+	// 			setList(data.todos)
+	// 		}
+	// 		  // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+	// 		  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+	// 	  })
+	// 	  .catch(error => {
+	// 		  // Manejo de errores
+	// 		  console.log(error);
+	// 	  });
+	// }
+	const addNewTask = () =>{
+		fetch('https://playground.4geeks.com/todo/todos/Adolfo1996', {
+			method: "POST",
+			body: JSON.stringify({
+				"label": newTodo,
+				"is_done": false
+			  }),
+			headers: {
+			  "Content-Type": "application/json"
+			}
+		  })
+		  .then(resp => {
+			if(resp.ok){
+				getList()
+			}
+			  		  })
+		  
+		  .catch(error => {
+			  // Manejo de errores
+			  console.log(error);
+		  });
+	}
+
+	useEffect(()=>{
+		getList()
+	},[])
+
+	
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">
 				Fetch and react
 			</h1>
 			<div>
-				<input type="text" onChange={handleChange} />
-				<button onClick={holaClickeame}  >
+				<input type="text" value={newTodo} onChange={handleChange} />
+				<button onClick={addNewTask}  >
 					Add task
 				</button>
 			</div>
 			<p>New Task:{newTodo} </p>
 			<ul>
-				{list.map((list, indice) => {
+				{list.map((item, index) => {
 					return (
-						<li>
-							{list}<button onClick={() => deleteTask()}>Erase</button>
+						<li key={index}>
+							{item.label}<button onClick={() => deleteTask(index)}>Erase</button>
 
 						</li>
 					)
