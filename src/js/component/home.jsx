26c92729d6a5cfc7300e197a68b9f55e3bfd85f2
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-09-18 02:28:32 +0000

    Terminado.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -31,92 +31,83 @@ const Home = () => {
 	const createUser = () => {
 		fetch('https://playground.4geeks.com/todo/users/Adolfo1996', {
 			method: "POST",
-			
-		  })
-		  .then(resp => {
-			  console.log(resp.ok); // Será true si la respuesta es exitosa
-			  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-			  console.log(resp.text()); // Intentará devolver el resultado exacto como string
-			  return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-		  })
-		  .then(data => {
-			  // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-			  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-		  })
-		  .catch(error => {
-			  // Manejo de errores
-			  console.log(error);
-		  });
-	} 
-	const getList = async () =>{
+
+		})
+			.then(resp => {
+				console.log(resp.ok); // Será true si la respuesta es exitosa
+				console.log(resp.status); // El código de estado 200, 300, 400, etc.
+				console.log(resp.text()); // Intentará devolver el resultado exacto como string
+				return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+			})
+			.then(data => {
+				// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				// Manejo de errores
+				console.log(error);
+			});
+	}
+	const getList = async () => {
 		try {
-			const response = await fetch ('https://playground.4geeks.com/todo/users/Adolfo1996')
+			const response = await fetch('https://playground.4geeks.com/todo/users/Adolfo1996')
 			const data = await response.json()
-			if(response.status==404) {
+			if (response.status == 404) {
 				createUser()
-				
+
 			}
-		    if(response.ok){
+			if (response.ok) {
 				setList(data.todos)
 			}
-		} catch (error){}
+		} catch (error) { }
 	}
 
-	//
-	// const getList = () => {
-	// 	fetch('https://playground.4geeks.com/todo/users/Adolfo1996')
-	// 	  .then(resp => {
-	// 		if(resp.status ==404) {
-	// 			createUser()
-	// 		}
-	// 		if(resp.ok){
-	// 			return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+	const deleteUser = async (id) => {
 
-	// 		} 
-	// 		  console.log(resp.ok); // Será true si la respuesta es exitosa
-	// 		  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-	// 		  console.log(resp.text()); // Intentará devolver el resultado exacto como string
-	// 	  })
-	// 	  .then(data => {
-	// 		if(data){
-	// 			setList(data.todos)
-	// 		}
-	// 		  // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-	// 		  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-	// 	  })
-	// 	  .catch(error => {
-	// 		  // Manejo de errores
-	// 		  console.log(error);
-	// 	  });
-	// }
-	const addNewTask = () =>{
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+			})
+
+			getList()
+
+		}
+		catch (error) {
+			console.log(error)
+
+		}
+	}
+
+	
+	const addNewTask = () => {
 		fetch('https://playground.4geeks.com/todo/todos/Adolfo1996', {
 			method: "POST",
 			body: JSON.stringify({
 				"label": newTodo,
 				"is_done": false
-			  }),
+			}),
 			headers: {
-			  "Content-Type": "application/json"
+				"Content-Type": "application/json"
 			}
-		  })
-		  .then(resp => {
-			if(resp.ok){
-				getList()
-			}
-			  		  })
-		  
-		  .catch(error => {
-			  // Manejo de errores
-			  console.log(error);
-		  });
+		})
+			.then(resp => {
+				if (resp.ok) {
+					getList()
+				}
+			})
+
+			.catch(error => {
+				// Manejo de errores
+				console.log(error);
+			});
 	}
 
-	useEffect(()=>{
+	useEffect(() => {
 		getList()
-	},[])
+	}, [])
 
-	
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">
@@ -124,7 +115,7 @@ const Home = () => {
 			</h1>
 			<div>
 				<input type="text" value={newTodo} onChange={handleChange} />
-				<button onClick={addNewTask}  >
+				<button type="button" class="btn btn-success" onClick={addNewTask}  >
 					Add task
 				</button>
 			</div>
@@ -133,7 +124,7 @@ const Home = () => {
 				{list.map((item, index) => {
 					return (
 						<li key={index}>
-							{item.label}<button onClick={() => deleteTask(index)}>Erase</button>
+							{item.label}<button type="button" class="btn btn-danger" onClick={() => deleteUser(item.id)}>Erase</button>
 
 						</li>
 					)
